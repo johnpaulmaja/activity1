165b8776adfc8c9c882c6a9c038454967ded8200
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-01-28 21:23:21 +0800

    Update majarss.php

--- majarss.php
+++ majarss.php
@@ -11,11 +11,11 @@ $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
 while($record= mysqli_fetch_assoc($query)) {
     extract($record);
-    $rss .= '<article>';
+    $rss .= '<book>';
     $rss .= '<title>' . $title . '</title>';
-    $rss .= '<link>' . $link . '</link>';
+    $rss .= '<author>' . $link . '</author>';
     $rss .= '<description>' . $description . '</description>';
-    $rss .= '</article>';
+    $rss .= '</book>';
 }
 $rss .= '</channel>';
 $rss .= '</rss>';
