7d10ba2823f539e12391d8d9276ea993d0b93f4b
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-01-28 11:19:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 // Create connection
 $con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com","admin","root1234","db_1820681");
-$db = mysqli_select_db($con,"rss");
 // Check connection
  if (mysqli_connect_errno($con)) {
  echo "Database connection failed!: " . mysqli_connect_error();
