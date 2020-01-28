9b17d0b2a47c0acdd387d68e85c3809b23f4826d
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-01-28 11:43:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $rss .= '<rss version="2.0">';
 $rss .= '<channel>';
 
 $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820681") or die (mysqli_error($connect));
-$sql = "SELECT * FROM tbl.rss_info";
+$sql = "SELECT * FROM rss_info";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
 
