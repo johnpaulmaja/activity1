1493ebae666a6873ab43db41776cd37a5bc2964d
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 15:35:48 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -4,20 +4,18 @@
     $rssfeed .= '<rss version="2.0">';
     $rssfeed .= '<channel>';
 
-    $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820677") or die(mysqli_error($connect));
-    $sql = "SELECT * FROM tblmonsters";
+    $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820681") or die(mysqli_error($connect));
+    $sql = "SELECT * FROM tblbooks";
     $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
     while($row=mysqli_fetch_assoc($query)) {
         extract($row);
 
-        $rssfeed .= '<Monsters>';
-        $rssfeed .= '<Monster>' . $Monster . '</Monster>';
-        $rssfeed .= '<Classification>' . $Classification . '</Classification>';
-        $rssfeed .= '<Immunity>' . $Immunity . '</Immunity>';
-        $rssfeed .= '<Tactics>' . $Tactics . '</Tactics>';
-        $rssfeed .= '<Occurence>' . $Occurence . '</Occurence>';
-        $rssfeed .= '</Monsters>';
+        $rssfeed .= '<Books>';
+        $rssfeed .= '<Title>' . $Title . '</Title>';
+        $rssfeed .= '<Author>' . $Author . '</Author>';
+        $rssfeed .= '<Description>' . $Description . '</Description>';
+        $rssfeed .= '</Books>';
     }
 
     $rssfeed .= '</channel>';
