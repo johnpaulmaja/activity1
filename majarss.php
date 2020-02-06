d88842892d963fbbbf4569d341d1f1b6516f69a5
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 14:26:41 +0800

    Update majarss.php

--- majarss.php
+++ majarss.php
@@ -1,24 +1,25 @@
-<?php 
+<?php
 
-$rss= '<?xml version="1.0" encoding="UTF-8"?>';
-$rss .= '<rss version="2.0">';
-$rss .= '<channel>';
+    $rssfeed = '<?xml version="1.0" encoding="UTF-8"?>';
+    $rssfeed .= '<rss version="2.0">';
+    $rssfeed .= '<channel>';
 
-$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820681") or die (mysqli_error($connect));
-$sql = "SELECT * FROM tblBook";
-$query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
+    $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820681") or die(mysqli_error($connect));
+    $sql = "SELECT * FROM tblBook";
+    $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
+    while($row=mysqli_fetch_assoc($query)) {
+        extract($row);
 
-while($record= mysqli_fetch_assoc($query)) {
-    extract($record);
-    $rss .= '<book>';
-    $rss .= '<title>' . $title . '</title>';
-    $rss .= '<author>' . $author . '</author>';
-    $rss .= '<description>' . $description . '</description>';
-    $rss .= '</book>';
-}
-$rss .= '</channel>';
-$rss .= '</rss>';
+        $rssfeed .= '<books>';
+        $rssfeed .= '<title>' . $title . '</title>';
+        $rssfeed .= '<author>' . $author . '</author>';
+        $rssfeed .= '<description >' . $description . '</description>';
+        $rssfeed .= '</books>';
+    }
 
-echo $rss; 
-?>
+    $rssfeed .= '</channel>';
+    $rssfeed .= '</rss>';
+
+    echo $rssfeed;
+?>    
