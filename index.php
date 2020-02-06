22f8fa3f2b1a5c038928c5e7f6c395537fbfbe47
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 14:30:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 
 <?php
- $domOBJ = new DOMDocument();
- $domOBJ->load("https://maja-act-1.herokuapp.com/majarss.php");//XML page URL
- 
- $content = $domOBJ->getElementsByTagName("book");
- 
- ?>
- <ul>
-    <?php
- foreach( $content as $data )
- {
-   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-   $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
-   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-  
-    echo "<li>$title
-            <ul>
-                <li>$author</li>
-                <li>$description</li>
-            </ul>
-        </li>";
- 
- }
+       $domOBJ = new DOMDocument();
+       $domOBJ->load("https://maja-act-1.herokuapp.com/majarss.php");//XML page URL
+
+       $content = $domOBJ->getElementsByTagName("books");
 ?>
-</ul>
+    
+       
+       <h1>Monsters</h1>
+           
+       
+        <?php
+              foreach($content as $data)
+        {          
+           $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+           $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+           $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+           echo "<ul><h2>$title</h2>
+                    <ul>
+                        <li>Author: $author</li>
+                        <li>Description: $description</li>
+                           
+                    </ul>
+                </ul>";
+              }
+?>
